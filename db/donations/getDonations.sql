8e6348a9fa3c60e433be9729149b6b79eb061b5f
Rahime Tayfun <suheyladurna@gmail.com>
2020-04-02 16:53:58 -0500

    finished donation search functionality and did some styling

--- db/donations/getDonations.sql
+++ db/donations/getDonations.sql
@@ -1,3 +1,3 @@
-SELECT distinct d.*, dp.donation_photo FROM  donations d
+SELECT distinct on (d.donation_id) d.post_date,d.*, dp.donation_photo FROM  donations d
 inner join donation_photos dp ON dp.donation_id = d.donation_id
-order by d.post_date desc;
+order by d.donation_id desc;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,28 +2,62 @@ import React from "react";
 import Categories from "../components/Categories";
 import {connect} from 'react-redux';
 import { getDonations } from "../redux/reducers/donationReducer";
+import Header from './Header';
+import Footer from './Footer';
+import {MdSearch} from 'react-icons/md';
+
 
 class Home extends React.Component {
+  state={search:''}
     componentDidMount(){
         this.props.getDonations();
     }
+  handleChange=(e)=>{
+    this.setState({[e.target.name]:e.target.value})
+  }
   render() {
-    const mappedDonation = this.props.donations.map(el=>{
+    const {search} = this.state; 
+    let mappedDonation = this.props.donations.map((el,i)=>{
         return(
-            <div key={el.donation_id} style={{'border':'1px solid black','width':'40vw'}}>
+            <div key={el.donation_title+i} className="donation-card"
+            style={{ border: '1px solid black', width: '40vw' }}>
+                <img src={el.donation_photo} alt="donation" width='200px'/>
                 <p>{el.donation_title}</p>
                 <p>{el.post_location}</p>
-                <img src={el.donation_photo} width="200px"/>
             </div>
         )
-
     })
-      console.log(this.props.donations)
+    let filteredDonation = this.props.donations.filter((el)=>{
+      return el.donation_title.toLowerCase().includes(search.toLowerCase())
+    }).map((el,i)=>{
+      return(
+        <div key={el.donation_title+i} className="donation-card"
+        style={{ border: '1px solid black', width: '40vw' }}>
+            <img src={el.donation_photo} alt="donation" width='200px'/>
+            <div>
+            <p>{el.donation_title}</p>
+            <p>{el.post_location}</p>
+
+            </div>
+        </div>
+      )
+    });
     return (
-      <div>
-        <h3>Home Component</h3>
-        {mappedDonation}
+      <div className="parent-container">
+        <Header/>
         <Categories />
+        <div className="donation-container">
+          <div className="search-container">
+          <div className="search">
+            <MdSearch size='25px'/>
+            <input placeholder="search donations" id="input" name="search" onChange={this.handleChange}/>
+          </div>
+          </div>
+            <div className="donation-card-container">
+            {!search? mappedDonation : filteredDonation}
+            </div>
+        </div>
+        <Footer/>
       </div>
     );
   }
