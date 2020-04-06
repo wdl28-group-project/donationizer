ae653147c8253fb753657bfd9fe554799e89bd72
John Langdon <johnlangdon@johns-mbp.home>
2020-04-06 14:20:58 -0500

    updated

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,42 +1,105 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { editUser } from '../redux/reducers/authReducer';
+import React from "react";
+import { connect } from "react-redux";
+import { editUser } from "../redux/reducers/authReducer";
+import "./stylescomponent/Profile.scss";
 
 class Profile extends React.Component {
+  constructor() {
+    // let { username, password, loginUser } = props;
+    super();
+    this.state = {
+      username: "",
+      location: "",
+      url: "",
+      password: "",
+      currentPassword: "",
+      newPassword: "",
+      confirmPassword: ""
+    };
+  }
+  //make state here to hold the inputs
+  handleInput = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    constructor() {
-        super();
-        this.state = {
-            username: '',
-            location: '',
-            url: ''
-        }
-    }
-//make state here to hold the inputs 
-    handleInput = e => {
-        this.setState({ [e.target.name]: e.target.value} )
-    }
+  // make method like handle input & handle click
+  handleClick = () => {
+    const { username, location, url } = this.state;
 
-// make method like handle input & handle click
-    handleClick = () => {
-        const { username, location, url } = this.state;
+    this.props.editUser({ username, location, url });
+  };
 
-        this.props.editUser({ username, location, url });
-    }
-
-    render(){
-        return(
-            <div>
-                <div>Profile</div>
-                <input onChange={this.handleInput} name='username' placeholder="Username" />
-                <input onChange={this.handleInput} name='location' placeholder="Location" />
-                <input onChange={this.handleInput} name='url' placeholder="URL" />
-                <button onClick={this.handleClick}>Save changes</button>
+  render() {
+    return (
+      <div className="profile">
+        <div className="profile-header">Tap on photo to change it</div>
+        <div className="profile-content">
+          <div className="userName">
+            <div className="userName-text">New Username</div>
+            <input
+            className="input-username"
+              onChange={this.handleInput}
+              name="username"
+              placeholder="new username"
+            />
+            <div className="userName-text">New Location</div>
+            <input
+            className="input-username"
+              onChange={this.handleInput}
+              name="location"
+              placeholder="new location"
+            />
+            <div className="userName-text">URL</div>
+            <input className="input-username" onChange={this.handleInput} name="url" placeholder="paste image URL" />
+            <div className="button-wrapper">
+            <button className="save-changes" onClick={this.handleUserName}>Save changes</button>
             </div>
-        )
+
+          </div>
+          <div className="passWord">
+              <div className="change-password">Change Password</div>
+              <div className="userName-text">Current Password</div>
+            <input
+            className="input-username"
+              onChange={this.handleInput}
+              name="current-password"
+              placeholder="current password"
+            />
+            <div className="userName-text">New Password</div>
+            <input
+            className="input-username"
+              onChange={this.handleInput}
+              name="new-password"
+              placeholder="new password"
+            />
+            <div className="userName-text">Confirm Password</div>
+            <input
+            className="input-username"
+              onChange={this.handleInput}
+              name="confirm-password"
+              placeholder="confirm password"
+            />
+            <div className="button-wrapper">
+            <button className="save-changes" onClick={this.handlePassword}>Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+    let { user, username, password, loading } = state.authReducer;
+    console.log(state)
+    return{
+        user,
+        username,
+        password,
+        loading
     }
 }
 
 export default connect(null, {
-    editUser
+  editUser,
 })(Profile);
