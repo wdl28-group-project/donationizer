185487b27c3118280001f4fc8089b73b332f295c
ArthrVolfson <arthurvolfson@gmail.com>
2020-04-08 11:17:41 -0500

    updated commits 04.08.2020

--- src/components/DonationDetails.js
+++ src/components/DonationDetails.js
@@ -1,109 +1,72 @@
-<<<<<<< HEAD
 import React from 'react';
 import { connect } from 'react-redux';
 import {
   getDonations,
   getDonationsdetail,
+  updateViewCount,
 } from '../redux/reducers/donationReducer';
 import { Link, Redirect } from 'react-router-dom';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import './stylescomponent/Slider.scss';
 import axios from 'axios';
-=======
-import React from "react";
-import { connect } from "react-redux";
-import { getDonations, getDonationsdetail, updateViewCount} from "../redux/reducers/donationReducer";
-import { Link, Redirect } from "react-router-dom";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-import "./stylescomponent/Slider.scss"
-import axios from "axios";
-import {IoMdEye, IoIosHeart, IoMdMail } from "react-icons/io";
-
->>>>>>> master
+import { IoMdEye, IoIosHeart, IoMdMail } from 'react-icons/io';
 
 class DonationDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-<<<<<<< HEAD
       pics: [],
     };
   }
 
   componentDidMount() {
-    axios
-      .get(`/api/donation/${this.props.details.donation_id}/photos`)
-      .then((res) => {
-        console.log(res);
-        var url = [];
-        for (var j = 0; j < res.data.length; j++) {
-          url.push(res.data[j].donation_photo);
-          this.setState({ pics: url });
-        }
-      })
-      .catch((res) => {
-        console.log(res);
-      });
+    if (this.props.detail.length === 0) {
+      return console.log();
+    } else if (this.props.detail.length > 0) {
+      updateViewCount(this.props.details.donation_id);
+      axios
+        .get(`/api/donation/${this.props.details.donation_id}/photos`)
+        .then((res) => {
+          console.log(res);
+          var url = [];
+          for (var j = 0; j < res.data.length; j++) {
+            if (url.includes(res.data[j].donation_photo) === false) {
+              url.push(res.data[j].donation_photo);
+            }
+            this.setState({ pics: url });
+          }
+        })
+        .catch((res) => {
+          console.log(res);
+        });
+    }
   }
 
   render() {
     const mappedPhotos = this.state.pics.map((post, indx) => {
-      console.log(post);
       return (
-        <div key={indx} className='w-event-container'>
-          <img
-            style={{ minheight: '10vh', maxHeight: '20vh' }}
-            src={post}
-            alt='Alt text'
-            className='w-event-image-container'
-          />
-        </div>
-=======
-     pics: []
-   
-    }; 
-  }
-
-  componentDidMount() {
-  if(this.props.detail.length === 0){ return console.log()} else if(this.props.detail.length>0){  
-    updateViewCount(this.props.details.donation_id) 
-    axios.get(`/api/donation/${this.props.details.donation_id}/photos`)
-    .then(res=>{ console.log(res)
-        var url=[]
-        for (var j=0; j<res.data.length; j++) {
-          if (url.includes(res.data[j].donation_photo) === false){
-            url.push(res.data[j].donation_photo);}
-            this.setState({pics: url})
-        }
-    })
-    .catch(res=>{console.log(res)})
-  }}
-
-  
-
-  render() {
-    const mappedPhotos =  this.state.pics.map((post, indx) => {
-        return (
-            <img
-              style={{ minheight: "100vh", maxHeight: "100vh", minWidth: "50vw", maxWidth: "60vw" }}
-              src={post}
-              alt="Alt text"
-              className="w-event-image-container"
-            />
->>>>>>> master
+        <img
+          style={{
+            minheight: '100vh',
+            maxHeight: '100vh',
+            minWidth: '50vw',
+            maxWidth: '60vw',
+          }}
+          src={post}
+          alt='Alt text'
+          className='w-event-image-container'
+        />
       );
     });
     const responsive = {
-<<<<<<< HEAD
       desktop: {
         breakpoint: { max: 3000, min: 1100 },
-        items: 3,
+        items: 1,
       },
       tablet: {
         breakpoint: { max: 1100, min: 690 },
-        items: 3,
+        items: 1,
       },
       phone: {
         breakpoint: { max: 690, min: 405 },
@@ -114,46 +77,16 @@ class DonationDetails extends React.Component {
         items: 1,
       },
     };
-=======
-        desktop: {
-          breakpoint: { max:3000, min:1100 },
-          items: 1
-        },
-        tablet: {
-          breakpoint: { max:1100, min: 690 },
-          items: 1
-        },
-        phone:{
-          breakpoint: { max:690, min:405 },
-          items: 1
-        },
-        xphone:{
-          breakpoint: { max:405, min:0 },
-          items: 1
-        }
-    
-      };
-
-      
->>>>>>> master
 
     if (this.props.detail.length === 0) {
       return <Redirect to='/' />;
     } else if (this.props.detail.length > 0) {
       return (
-<<<<<<< HEAD
         <div>
           DonationDetails
           <Carousel responsive={responsive} className='w-event-parent'>
             {mappedPhotos}
           </Carousel>
-=======
-          <div >
-
-          DonationDetails
-          <Carousel responsive={responsive} className="w-event-parent">
-                {mappedPhotos}
-            </Carousel>
           <div>
             <div>
               {this.props.details.view_count}
@@ -162,7 +95,6 @@ class DonationDetails extends React.Component {
             <IoIosHeart></IoIosHeart>
             <IoMdMail></IoMdMail>
           </div>
->>>>>>> master
           <h1>{this.props.details.donation_title}</h1>
           <h2>{this.props.details.category_name}</h2>
           <h3>{this.props.details.donation_desc}</h3>
@@ -187,6 +119,8 @@ const mapStateToProps = (reduxState) => {
   };
 };
 
-export default connect(mapStateToProps, { getDonationsdetail, getDonations, updateViewCount })(
-  DonationDetails
-);
+export default connect(mapStateToProps, {
+  getDonationsdetail,
+  getDonations,
+  updateViewCount,
+})(DonationDetails);
