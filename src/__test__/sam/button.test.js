86dabb7eec6c4e20851b61060b63ac285ef2294a
samFils-Aime <samfilsaime@hotmail.com>
2020-04-09 12:05:44 -0500

    update

---
+++ src/__test__/sam/button.test.js
@@ -0,0 +1,9 @@
+import  React, { Component } from 'react'
+import reactDOM from "react-dom"
+import {render} from "@testing-library/react"
+import MyDonations from "../../components/Profile/Tabs/MyDonations"
+
+it("renders", ()=>{
+    const div = document.createElement("button");
+    reactDOM.render(<button></button>)
+})
